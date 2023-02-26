ccb35c2ca5295add97f624692c0a50560d19da5d
Shurti Nigam <shruti26nigam>
2023-02-26 10:32:03 +0530

    star.ipy

--- game1.py
+++ game1.py
@@ -1,7 +1,10 @@
 import pgzrun 
 
+# add bgm music
+music.play('bg')
+
 b = Rect((50,50), (100,50))
-vx, vy = 3, 5                   #global variable
+vx, vy = 3, 5                  #global variable
 
 def draw():
     screen.fill('black')
@@ -13,8 +16,11 @@ def update():
     b.y += vx
     if b.right >800 or b.left < 0:
         vx = -vx
+        sounds.s1.play()
+        
     if b.bottom > 600 or b.top < 0:
         vy = -vy
+        sounds.s1.play()
 
 # outside of all function
 pgzrun.go()
